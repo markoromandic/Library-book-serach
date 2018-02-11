022bf62b93ea5c2bb49976f7de065ea46c623876
Marko Romandic <markoromandic11@gmail.com>
2018-02-11 20:02:18 +0100

    Updated parsing code

--- parser.py
+++ parser.py
@@ -1,36 +1,71 @@
 import re
 
-count = 0
-def parse_line(ln=''):
+count = 1
+
+
+class Book(object):
+    def __init__(self, book_name='', author_name=''):
+        self.book_name = book_name
+        self.author_name = author_name
+
+    def __str__(self):
+        return 'Naziv knjige: ' + self.book_name + ' Author: ' + self.author_name
+
+
+def parse_book_file(ln=''):
     global count
-    book = re.split(chr(30) + '200..' + chr(31) + 'a', ln)
-    name = re.split(chr(30) + '700.1' + chr(31) + '4070' + chr(31) + 'a', ln)
-    numReprinted = re.split(chr(30) + '205' + '..' + chr(31) + 'a', ln)
 
-    count += + 1
+    book = Book()
 
-    if len(book) > 1:
-        book = re.split(chr(31), book[1])
-        # print(ln[0])
-    if len(name) > 1:
-        firstPart = re.split(chr(31) + 'b', name[1])
-        if len(firstPart) < 2:
-            firstPart = re.split(chr(31), name[1])
-            # print(firstPart)
-            firstPart = re.split(chr(30), firstPart[0])
-            # print(firstPart[0])
-            secondPart = re.split(chr(30), firstPart[0])
-        else:
-            secondPart = re.split(chr(30), firstPart[1])
-            if chr(31) in secondPart[0]:
-                secondPart = re.split(chr(31), secondPart[0])
-        print(count, firstPart[0], secondPart[0])
+    # PARSING BOOK NAME
+    book_name = re.split(chr(30) + '200..' + chr(31) + 'a', ln)
+
+    for i in range(0, len(book_name[1])):
+        if book_name[1][i] == chr(30) or book_name[1][i] == chr(31):
+            break
+        book.book_name = book.book_name + book_name[1][i]
+
+    # PARSING AUTHOR NAME
+    book_author_name = re.split(chr(30) + '700.1' + chr(31) + '4070' + chr(31) + 'a', ln)
+
+    if len(book_author_name) == 2:
+        book_author_name = re.split(chr(31) + 'b', book_author_name[1])
+
+        for i in range(0, len(book_author_name[0])):
+            if book_author_name[0][i] == chr(30):
+                break
+            elif book_author_name[0][i] == chr(31):
+                if book_author_name[0][i + 1] is not 'b':
+                    continue
+                book.author_name = book.author_name + ' '
+            book.author_name = book.author_name + book_author_name[0][i]
+
+        print(count, book)
+
+    count += +1
+
+
+
+def parse_line(ln=''):
+    firstPart = re.split(chr(31) + 'b', name[1])
+    if len(firstPart) < 2:
+        firstPart = re.split(chr(31), name[1])
+        # print(firstPart)
+        firstPart = re.split(chr(30), firstPart[0])
+        # print(firstPart[0])
+        secondPart = re.split(chr(30), firstPart[0])
+    else:
+        secondPart = re.split(chr(30), firstPart[1])
+        if chr(31) in secondPart[0]:
+            secondPart = re.split(chr(31), secondPart[0])
+    print(count, firstPart[0], secondPart[0])
 
     # if len(numReprinted) > 1:
     #     numReprinted = re.split(chr(31), numReprinted[1])
     #     print(numReprinted[0])
 
-
 with open(file='fajlovi/knjige.txt', encoding="utf8") as fp:
     for line in fp:
-        parse_line(ln=line)
+        parse_book_file(ln=line)
+
+
