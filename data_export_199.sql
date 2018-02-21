81a8bd3748c38bbb883bbf0087b23d0e59076031
Marko Romandic <markoromandic11@gmail.com>
2018-02-21 17:19:39 +0100

    Added files to ignore

---
+++ data_export_199.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f6a523140ba07c26b0a380e1953fcc2777f74d9b630fad503d295a74dd649c36
+size 442699
---
+++ parser.py
@@ -0,0 +1,237 @@
+import re
+import MySQLdb
+import time
+
+books = []
+prefixes = []
+prefix_names = []
+
+
+class Book(object):
+    def __init__(self):
+        self.records = []
+
+    def __str__(self):
+        print('START_BOOK_RECORDS')
+        for r in self.records:
+            print(r)
+        return 'END_BOOK_RECORDS'
+
+
+class Record(object):
+    def __init__(self, record_code='', first_indicator='',second_indicator=''):
+        self.record_code = record_code
+        self.first_indicator = first_indicator
+        self.second_indicator = second_indicator
+        self.subfields = []
+
+    def prepare_subfields(self):
+        line = ''
+
+        for s in self.subfields:
+            line = line + '| Subfield code: ' + s.subfield_code + ' Subfield data: ' + s.data
+
+        return line
+
+    def __str__(self):
+        return 'Record code: ' + self.record_code + ' First indicator: ' + self.first_indicator \
+               + ' Second indicator: ' + self.second_indicator + ' Subfields: ' + self.prepareSubfields()
+
+
+class SubfieldRecord(object):
+    def __init__(self, subfield_code='', data=''):
+        self.subfield_code = subfield_code
+        self.data = data
+
+    def __str__(self):
+        return 'Subfield code: ' + self.subfield_code + ' Data: ' + self.data
+
+
+class PrefixName(object):
+    def __init__(self, prefix_code='', prefix_name=''):
+        self.prefix_code = prefix_code
+        self.prefix_name = prefix_name
+
+    def __str__(self):
+        return 'Prefix code: ' + self.prefix_code + ' Prefix name: ' + self.prefix_name
+
+
+class Prefix(object):
+    def __init__(self, prefix_code='', field_code='', subfield_code=''):
+        self.prefix_code = prefix_code
+        self.field_code = field_code
+        self.subfield_code = subfield_code
+
+    def __str__(self):
+        return 'Prefix code: ' + self.prefix_code + ' Field code: ' + self.field_code + ' Subfield code: ' \
+               + self.subfield_code
+
+
+def parse_book_file(ln=''):
+    global books
+
+    new_book = Book()
+    books.append(new_book)
+
+    records = re.split(chr(30), ln)
+
+    for r in records:
+        subfields = re.split(chr(31), r)
+        new_record = Record(subfields[0][0:3], first_indicator=subfields[0][3], second_indicator=subfields[0][4])
+        new_book.records.append(new_record)
+
+        for i in range(1, len(subfields)):
+            if subfields[i][len(subfields[i]) - 1] is not '\n':
+                new_subfield = SubfieldRecord(subfield_code=subfields[i][0],
+                                              data=subfields[i][1:(len(subfields[i]))])
+            else:
+                new_subfield = SubfieldRecord(subfield_code=subfields[i][0],
+                                              data=subfields[i][1:(len(subfields[i]) - 1)])
+            new_record.subfields.append(new_subfield)
+
+
+def parse_prefix_file(ln=''):
+    global prefixes
+
+    ln = re.split('-', ln)
+
+    prefix = Prefix(prefix_code=ln[0][0:2], field_code=ln[1][0:3], subfield_code=ln[1][3])
+
+    prefixes.append(prefix)
+
+
+def parse_prefix_names_file(ln=''):
+    global prefix_names
+
+    ln = re.split('=', ln)
+
+    prefix_name = PrefixName(prefix_code=ln[0][0:2], prefix_name=ln[1][0:len(ln[1]) - 1])
+
+    prefix_names.append(prefix_name)
+
+
+with open(file='fajlovi/knjige.txt', encoding='utf 8') as fp:
+    for line in fp:
+        parse_book_file(ln=line)
+
+with open(file='fajlovi/prefiksi.txt', encoding='utf8') as fp:
+    for line in fp:
+        parse_prefix_file(ln=line)
+
+with open(file='fajlovi/PrefixNames_sr.properties', encoding='utf8') as fp:
+    for line in fp:
+        parse_prefix_names_file(ln=line)
+
+
+# for b in books:
+#     print(b)
+#
+# for p in prefixes:
+#     print(p)
+#
+# for p_n in prefix_names:
+#     print(p_n)
+
+def insert_prefixes_into_database(cursor):
+    global prefixes
+    add_prefix = ("INSERT INTO prefix "
+                    "(prefix_code, field_code, subfield_code ) "
+                    "VALUES (%(prefix_code)s, %(field_code)s, %(subfield_code)s)")
+    for prefix in prefixes:
+        data_prefix = {
+            'prefix_code': prefix.prefix_code,
+            'field_code': prefix.field_code,
+            'subfield_code': prefix.subfield_code,
+        }
+        try:
+            cursor.execute(add_prefix, data_prefix)
+        except MySQLdb.Error as err:
+            print("Something went wrong5: {}".format(err))
+            return
+
+
+def insert_prefix_names_into_database(cursor):
+    global prefix_names
+    add_prefix_names = ("INSERT INTO prefix_names "
+                    "(prefix_code, prefix_name ) "
+                    "VALUES (%(prefix_code)s, %(prefix_name)s)")
+
+    for prefix_name in prefix_names:
+        data_prefix_names = {
+            'prefix_code': prefix_name.prefix_code,
+            'prefix_name': prefix_name.prefix_name,
+        }
+        try:
+            cursor.execute(add_prefix_names, data_prefix_names)
+        except MySQLdb.Error as err:
+            print("Something went wrong6: {}".format(err))
+            return
+
+
+def add_record_into_database(record, id_book,cursor):
+    add_field = ("INSERT INTO field "
+                  "(code, id_book, first_indicator, second_indicator) "
+                  "VALUES (%(code)s, %(id_book)s, %(first_indicator)s, %(second_indicator)s)")
+    data_field = {
+        'code': record.record_code,
+        'id_book': id_book,
+        'first_indicator': record.first_indicator,
+        'second_indicator': record.second_indicator,
+    }
+    try:
+        cursor.execute(add_field, data_field)
+    except MySQLdb.Error as err:
+        print("Something went wrong2: {}".format(err))
+        return
+
+    id_field = cursor.lastrowid
+
+    add_subfield = ("INSERT INTO subfield "
+                    "(code, content, id_field ) "
+                    "VALUES (%s, %s, %s)")
+    values = []
+    for subfield in record.subfields:
+        values.append((subfield.subfield_code,subfield.data,id_field))
+    try:
+        cursor.executemany(add_subfield, values)
+    except MySQLdb.Error as err:
+        print("Something went wrong3: {}".format(err))
+        return
+
+
+def insert_books_into_database():
+    time_start = int(round(time.time() * 1000))
+    global books
+    book_count = 0
+    conn = MySQLdb.connect(host= "localhost",
+                        user="root",
+                        passwd="toor",
+                        db="library_unimarc",
+                        charset='utf8')
+    cursor = conn.cursor()
+    for book in books:
+        try:
+            cursor.execute("INSERT INTO BOOK () VALUES ()")
+        except MySQLdb.Error as err:
+            print("Something went wrong1: {}".format(err))
+            return
+
+        id_book = cursor.lastrowid
+        for record in book.records:
+            add_record_into_database(record=record, id_book=id_book, cursor=cursor)
+        book_count += 1
+        if book_count % 100 == 0:
+            print(book_count)
+    insert_prefix_names_into_database(cursor=cursor)
+    insert_prefixes_into_database(cursor=cursor)
+    try:
+        conn.commit()
+    except MySQLdb.Error as err:
+        print("Something went wrong4: {}".format(err))
+        conn.rollback()
+    conn.close()
+    total_time = int(round(time.time() * 1000)) - time_start
+    print(total_time)
+
+
+insert_books_into_database()
